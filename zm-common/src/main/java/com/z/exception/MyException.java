2c61aa1abf763312317fd11d59261af6ae00a992
zdd <dingding.zhao@dhc.com.cn>
2021-01-14 20:01:07 +0800

    添加模块

---
+++ zm-common/src/main/java/com/z/exception/MyException.java
@@ -0,0 +1,9 @@
+package com.z.exception;
+
+public class MyException extends RuntimeException {
+
+    public MyException(String msg){
+        super(msg);
+    }
+
+}
--- zm-system/src/main/java/com/z/controller/AccountController.java
+++ zm-system/src/main/java/com/z/controller/AccountController.java
@@ -1,5 +1,6 @@
 package com.z.controller;
 
+import com.z.exception.MyException;
 import com.z.service.AccountService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -19,6 +20,9 @@ public class AccountController {
     @GetMapping
     public List<Map<String, Object>> getAll(){
         List<Map<String, Object>> all = accountService.getAll();
+        if (all.size()==0){
+            throw new MyException("无数据");
+        }
         return all;
     }
 
